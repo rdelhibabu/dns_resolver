a87136948b48e629574212d1a684f63add82320d
rdelhibabu <rdelhibabu@gmail.com>
2021-09-16 15:15:23 +0530

    Ramkumar S P S comments reslove

--- lookup.rb
+++ lookup.rb
@@ -12,26 +12,25 @@ dns_raw = File.readlines("zone")
 
 def parse_dns(dns_raw)
   dns_raw.map { |line|
-    source_destination = line.chomp().split(", ")
-    [source_destination[1], source_destination[2]]
+    parts = line.chomp().split(", ")
+    [parts[1], parts[2]]
   }.to_h
 end
 
-def resolve(dns_records, lookup_chain, domain)
-  block = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
-  re = /\A#{block}\.#{block}\.#{block}\.#{block}\z/
+def resolve(dns_records, domain,lookup_array=[domain])
+  ip_range_regex = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
+  ip_address_regex = /\A#{ip_range_regex}\.#{ip_range_regex}\.#{ip_range_regex}\.#{ip_range_regex}\z/
   if dns_records[domain] == nil
     print "Error: record not found for "
-  elsif (re =~ dns_records[domain])
-    lookup_chain.push(dns_records[domain])
+  elsif (ip_address_regex =~ dns_records[domain])
+    lookup_array.push(dns_records[domain])
   else
-    lookup_chain.push(dns_records[domain])
-    resolve(dns_records, lookup_chain, dns_records[domain])
+    lookup_array.push(dns_records[domain])
+    resolve(dns_records,  dns_records[domain],lookup_array)
   end
-  lookup_chain
+  lookup_array
 end
 
 dns_records = parse_dns(dns_raw)
-lookup_chain = [domain]
-lookup_chain = resolve(dns_records, lookup_chain, domain)
+lookup_chain = resolve(dns_records,  domain)
 puts lookup_chain.join(" => ")
