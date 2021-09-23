09e1aaf0e07da446ba2fa5b3612cae2015f3dcba
rdelhibabu <rdelhibabu@gmail.com>
2021-09-23 15:26:18 +0530

    Change lookup method

--- lookup.rb
+++ lookup.rb
@@ -5,32 +5,46 @@ def get_command_line_argument
   end
   ARGV.first
 end
-
 domain = get_command_line_argument
 
 dns_raw = File.readlines("zone")
 
-def parse_dns(dns_raw)
-  dns_raw.map { |line|
-    parts = line.chomp().split(", ")
-    [parts[1], parts[2]]
-  }.to_h
+def parse_dns(raw)
+  raw = raw.map(&:strip)
+  raw = raw.reject { |line| line.empty? }
+  raw = raw.reject { |line| line.start_with?("#") }
+  recordlines = raw.map { |line| line.strip.split(", ") }
+  recordlines = recordlines.reject { |record| record.length != 3 }
+
+  dns_records = recordlines.each_with_object({}) do |record, records| records[record[1]] =
+    { type: record.first, target: record.last }   end
+  dns_records
 end
 
-def resolve(dns_records, domain,lookup_array=[domain])
-  ip_range_regex = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
-  ip_address_regex = /\A#{ip_range_regex}\.#{ip_range_regex}\.#{ip_range_regex}\.#{ip_range_regex}\z/
-  if dns_records[domain] == nil
-    print "Error: record not found for "
-  elsif (ip_address_regex =~ dns_records[domain])
-    lookup_array.push(dns_records[domain])
-  else
-    lookup_array.push(dns_records[domain])
-    resolve(dns_records,  dns_records[domain],lookup_array)
+def resolve(dns_records, lookup_chain, domain)
+  matched_dns = dns_records[domain]
+
+  if (!matched_dns)
+    lookup_chain << "Error: Record not found for " + domain
+    return lookup_chain
   end
-  lookup_array
+
+  if (matched_dns[:type] == "A")
+    lookup_chain << matched_dns[:target]
+    return lookup_chain
+  end
+
+  if (matched_dns[:type] == "CNAME")
+    lookup_chain << matched_dns[:target]
+    resolve(dns_records, lookup_chain, matched_dns[:target])
+    return lookup_chain
+  end
+
+  lookup_chain << "Invalid record type for " + domain
+  return lookup_chain
 end
 
 dns_records = parse_dns(dns_raw)
-lookup_chain = resolve(dns_records,  domain)
+lookup_chain = [domain]
+lookup_chain = resolve(dns_records, lookup_chain, domain)
 puts lookup_chain.join(" => ")
